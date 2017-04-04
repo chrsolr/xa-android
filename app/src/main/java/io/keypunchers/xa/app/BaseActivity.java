6ed780c7968cb19437e8a2168466707468ef5597
christiansoler <iamrelos@gmail.com>
2017-04-04 16:11:09 -0500

    Updated

--- app/src/main/java/io/keypunchers/xa/app/BaseActivity.java
+++ app/src/main/java/io/keypunchers/xa/app/BaseActivity.java
@@ -1,9 +1,6 @@
 package io.keypunchers.xa.app;
 
-import android.content.SharedPreferences;
-import android.content.res.Configuration;
 import android.os.Bundle;
-import android.preference.PreferenceManager;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -11,25 +8,11 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Gravity;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ListView;
 
 import io.keypunchers.xa.R;
-import io.keypunchers.xa.adapters.NavigationDrawerAdapter;
 
 public class BaseActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
-    private final String PREF_USER_LEARNED_DRAWER = "USER_LEARNED_DRAWER";
-    private final String STATE_SELECTED_POSITION = "CURRENT_SELECTED_POSITION";
-    private boolean mUserLearnedDrawer = false;
-    private int mCurrentSelectedPosition = 2;
-    private String[] mDrawerTitles;
-    private DrawerLayout mDrawerLayout;
-    private ListView mDrawerListView;
-    private ActionBarDrawerToggle mDrawerToggle;
-    private boolean displayMenu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,46 +22,11 @@ public class BaseActivity extends AppCompatActivity implements NavigationView.On
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        getSupportActionBar().getThemedContext();
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
 
-        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
-        mUserLearnedDrawer = prefs.getBoolean(PREF_USER_LEARNED_DRAWER, false);
-
-        if (savedInstanceState != null) {
-            mCurrentSelectedPosition = savedInstanceState.getInt(STATE_SELECTED_POSITION);
-        }
-
-        mDrawerTitles = getResources().getStringArray(R.array.drawer_titles);
-
-        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
-
-        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        mDrawerLayout.addDrawerListener(mDrawerToggle);
-        mDrawerToggle.syncState();
-
-        if (savedInstanceState != null) {
-            //TODO: Select fragment
-        }
-
-
-        showOrHideMenu(true);
-    }
-
-    @Override
-    protected void onPostCreate(Bundle savedInstanceState) {
-        super.onPostCreate(savedInstanceState);
-        mDrawerToggle.syncState();
-    }
-
-    @Override
-    public void onConfigurationChanged(Configuration newConfig) {
-        super.onConfigurationChanged(newConfig);
-        mDrawerToggle.onConfigurationChanged(newConfig);
-    }
-
-    private void showOrHideMenu(boolean display) {
-        displayMenu = display;
-        supportInvalidateOptionsMenu();
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
     }
 
     @Override
