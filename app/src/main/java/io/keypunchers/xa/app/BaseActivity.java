640298019ee59c9e40a42c661906cc06186a5baa
christiansoler <iamrelos@gmail.com>
2017-04-04 13:59:56 -0500

    Updated

--- app/src/main/java/io/keypunchers/xa/app/BaseActivity.java
+++ app/src/main/java/io/keypunchers/xa/app/BaseActivity.java
@@ -1,12 +1,20 @@
 package io.keypunchers.xa.app;
 
 import android.content.SharedPreferences;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
 
 import io.keypunchers.xa.R;
+import io.keypunchers.xa.adapters.NavigationDrawerAdapter;
 
 public class BaseActivity extends AppCompatActivity {
     private final String PREF_USER_LEARNED_DRAWER = "USER_LEARNED_DRAWER";
@@ -14,6 +22,10 @@ public class BaseActivity extends AppCompatActivity {
     private boolean mUserLearnedDrawer = false;
     private int mCurrentSelectedPosition = 2;
     private String[] mDrawerTitles;
+    private DrawerLayout mDrawerLayout;
+    private ListView mDrawerListView;
+    private ActionBarDrawerToggle mDrawerToggle;
+    private boolean displayMenu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,5 +45,42 @@ public class BaseActivity extends AppCompatActivity {
         }
 
         mDrawerTitles = getResources().getStringArray(R.array.drawer_titles);
+
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mDrawerListView = (ListView) findViewById(R.id.drawer_list_view);
+
+        mDrawerListView.setAdapter(new NavigationDrawerAdapter(this, mDrawerTitles));
+        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        mDrawerLayout.addDrawerListener(mDrawerToggle);
+
+        if (savedInstanceState != null) {
+            //TODO: Select fragment
+        }
+
+        mDrawerListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                //TODO: Select fragment
+            }
+        });
+
+        showOrHideMenu(true);
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        mDrawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        mDrawerToggle.onConfigurationChanged(newConfig);
+    }
+
+    private void showOrHideMenu(boolean display) {
+        displayMenu = display;
+        supportInvalidateOptionsMenu();
     }
 }
