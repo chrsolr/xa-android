8c4acbabd4794c88644413a1a849a5e996548541
Christian Soler <iamrelos@gmail.com>
2017-04-27 14:29:24 -0500

    Create AchievementsListAdapter.java

---
+++ app/src/main/java/io/keypunchers/xa/adapters/AchievementsListAdapter.java
@@ -0,0 +1,49 @@
+package io.keypunchers.xa.adapters;
+
+public class AchievementsListAdapter extends RecyclerView.Adapter<AchievementsListAdapter.ViewHolder> {
+    private Context mContext;
+    private ArrayList<Achievement> mData;
+
+    public AchievementsListAdapter(Context context, ArrayList<Achievement> data) {
+        mContext = context;
+        mData = data;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        Context context = parent.getContext();
+        LayoutInflater inflater = LayoutInflater.from(context);
+
+        View view = inflater.inflate(R.layout.row_achievements, parent, false);
+
+        return new ViewHolder(view, mData, context);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        Achievement item = mData.get(position);
+    }
+
+    @Override
+    public int getItemCount() {
+        return mData.size();
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        private Context mContext;
+        private ArrayList<Achievement> mData;
+
+        public ViewHolder(View itemView, ArrayList<Achievement> data, Context context) {
+            super(itemView);
+            mData = data;
+            mContext = context;
+
+            itemView.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View v) {
+        
+        }
+    }
+}
