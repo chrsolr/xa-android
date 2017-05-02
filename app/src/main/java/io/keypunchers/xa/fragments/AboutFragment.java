7bdf5895de0c34cf295ecf37b0be98a150a27872
christiansoler <iamrelos@gmail.com>
2017-05-01 23:30:01 -0500

    Updated

--- app/src/main/java/io/keypunchers/xa/fragments/AboutFragment.java
+++ app/src/main/java/io/keypunchers/xa/fragments/AboutFragment.java
@@ -1,7 +1,6 @@
 package io.keypunchers.xa.fragments;
 
 
-import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -11,6 +10,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import io.keypunchers.xa.BuildConfig;
 import io.keypunchers.xa.R;
 
 /**
@@ -20,13 +20,20 @@ public class AboutFragment extends Fragment {
 
 
     public AboutFragment() {
-        // Required empty public constructor
     }
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_about, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        TextView mTvVersionName = (TextView) view.findViewById(R.id.tv_about_app_version_name);
+        mTvVersionName.setText(BuildConfig.VERSION_NAME);
     }
 
     @Override
