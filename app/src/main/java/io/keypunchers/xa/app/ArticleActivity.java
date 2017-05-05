5b914642d9ac435aba06d6ecb93e96a130719c8d
Christian Soler <iamrelos@gmail.com>
2017-05-04 19:36:39 -0500

    updated

--- app/src/main/java/io/keypunchers/xa/app/ArticleActivity.java
+++ app/src/main/java/io/keypunchers/xa/app/ArticleActivity.java
@@ -22,12 +22,25 @@ import io.keypunchers.xa.fragments.ImageListFragment;
 import io.keypunchers.xa.fragments.VideoListFragment;
 import io.keypunchers.xa.loaders.ArticleLoader;
 import io.keypunchers.xa.models.Article;
+import android.support.design.widget.FloatingActionButton;
+import android.view.View.OnClickListener;
+import android.view.View;
+import android.widget.Toast;
+import android.support.v7.app.AlertDialog;
+import android.widget.LinearLayout;
+import android.widget.EditText;
+import android.content.DialogInterface;
+import io.keypunchers.xa.loaders.SubmitArticleCommentLoader;
 
 public class ArticleActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Article> {
     private String BASE_URL;
     private int LOADER_ID;
     private Article mData;
     private String TAG = ArticleActivity.class.getSimpleName();
+
+	private ViewPager mViewPager;
+
+	private ViewPagerAdapter mAdapter;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -46,6 +59,45 @@ public class ArticleActivity extends AppCompatActivity implements LoaderManager.
 
         if (getIntent().getExtras() != null && getIntent().getExtras().containsKey("url"))
             BASE_URL = getIntent().getExtras().getString("url");
+
+		mViewPager = (ViewPager) findViewById(R.id.vp_article);
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tl_article);
+        tabLayout.setupWithViewPager(mViewPager);
+
+		FloatingActionButton mFab = (FloatingActionButton) findViewById(R.id.fab_article);
+		mFab.setOnClickListener(new OnClickListener(){
+				@Override
+				public void onClick(View view) {
+					AlertDialog.Builder mDialog = new AlertDialog.Builder(ArticleActivity.this);
+					mDialog.setTitle("Enter Comment");
+
+					final EditText mInput = new EditText(ArticleActivity.this);
+					LinearLayout.LayoutParams mLayoutParams = new LinearLayout.LayoutParams(
+						LinearLayout.LayoutParams.MATCH_PARENT,
+						LinearLayout.LayoutParams.MATCH_PARENT);
+
+					mInput.setLayoutParams(mLayoutParams);
+					mDialog.setView(mInput);
+
+					mDialog.setPositiveButton("Submit",
+						new DialogInterface.OnClickListener() {
+							public void onClick(DialogInterface dialog, int which) {
+								String comment = mInput.getText().toString();
+								postComment(comment);
+							}
+						});
+
+					mDialog.setNegativeButton("Cancel",
+						new DialogInterface.OnClickListener() {
+							public void onClick(DialogInterface dialog, int which) {
+								dialog.cancel();
+							}
+						});
+
+					mDialog.show();
+				}
+			});
 
         makeNetworkCall(savedInstanceState);
     }
@@ -112,23 +164,42 @@ public class ArticleActivity extends AppCompatActivity implements LoaderManager.
     }
 
     private void setupUI() {
-        ViewPager mViewPager = (ViewPager) findViewById(R.id.vp_article);
-
-        TabLayout tabLayout = (TabLayout) findViewById(R.id.tl_article);
-        tabLayout.setupWithViewPager(mViewPager);
-
-        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
-        adapter.addFragment(new ArticleFragment().newInstance(mData), "Article");
+        mAdapter = new ViewPagerAdapter(getSupportFragmentManager());
+        mAdapter.addFragment(new ArticleFragment().newInstance(mData), "Article");
 
         if (mData.getImageUrls().size() > 1)
-            adapter.addFragment(new ImageListFragment().newInstance(mData.getImageUrls()), "Images");
+            mAdapter.addFragment(new ImageListFragment().newInstance(mData.getImageUrls()), "Images");
 
         if (mData.getVideoUrls().size() > 0)
-            adapter.addFragment(new VideoListFragment().newInstance(mData.getVideoUrls()), "Videos");
+            mAdapter.addFragment(new VideoListFragment().newInstance(mData.getVideoUrls()), "Videos");
 
         if (mData.getComments().size() > 0)
-            adapter.addFragment(new CommentListFragment().newInstance(mData.getComments()), "Comments");
+            mAdapter.addFragment(new CommentListFragment().newInstance(mData.getComments()), "Comments");
 
-        mViewPager.setAdapter(adapter);
+        mViewPager.setAdapter(mAdapter);
+		mAdapter.notifyDataSetChanged();
+    }
+
+	private void postComment(final String comment) {
+        LoaderManager.LoaderCallbacks mSubmitCommentLoader = new LoaderManager.LoaderCallbacks<Boolean>() {
+
+			@Override
+			public Loader<Boolean> onCreateLoader(int id, Bundle args) {
+				return new SubmitArticleCommentLoader(getApplicationContext(), BASE_URL, comment);
+			}
+
+			@Override
+			public void onLoadFinished(Loader<Boolean> loader, Boolean data) {
+				if (data) {
+					getSupportLoaderManager().restartLoader(LOADER_ID, null, this);
+				}
+			}
+
+			@Override
+			public void onLoaderReset(Loader<Boolean> loader) {
+			}
+        };
+
+        getSupportLoaderManager().restartLoader(200, null, mSubmitCommentLoader);
     }
 }
---
+++ app/src/main/java/io/keypunchers/xa/loaders/SubmitArticleCommentLoader.java
@@ -0,0 +1,70 @@
+package io.keypunchers.xa.loaders;
+import android.content.Context;
+import android.support.v4.content.AsyncTaskLoader;
+import io.keypunchers.xa.misc.Common;
+import org.jsoup.Jsoup;
+import org.jsoup.Connection;
+import java.util.Locale;
+
+public class SubmitArticleCommentLoader extends AsyncTaskLoader<Boolean> {
+    private final String BASE_URL;
+	private String mComment;
+
+    public SubmitArticleCommentLoader(Context context, String url, String comment) {
+        super(context);
+        BASE_URL = url;
+		mComment = comment;
+    }
+
+    @Override
+    protected void onStartLoading() {
+        super.onStartLoading();
+		if (mComment == null || mComment.equals("")) {
+			super.deliverResult(false);
+		} else {
+            forceLoad();
+		}
+    }
+
+    @Override
+    public void deliverResult(Boolean data) {
+        if (isStarted()) {
+            super.deliverResult(data);
+        }
+    }
+
+    @Override
+    public Boolean loadInBackground() {
+
+        try {
+            String url = Common.BASE_URL + "/forum/login.php";
+            String userAgent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.96 Safari/537.36";
+
+            Connection.Response response = Jsoup.connect(url).userAgent(userAgent)
+				.method(Connection.Method.GET)
+				.execute();
+
+            response = Jsoup.connect(url)
+				.cookies(response.cookies())
+				.data("vb_login_username", "cs15")
+				.data("vb_login_password", "damian.l")
+				.data("do", "login")
+				.userAgent(userAgent)
+				.method(Connection.Method.POST)
+				.followRedirects(true)
+				.execute();
+
+            Jsoup.connect("http://www.xboxachievements.com/postComment.php?type=360news")
+				.data("newsID", Common.getNewsCommenstId(BASE_URL))
+				.data("username", "CS15")
+				.data("comment", String.format(Locale.US, "%s\n\n\nVia XA Android App", mComment))
+				.data("submit", "Submit")
+				.cookies(response.cookies())
+				.post();
+
+            return true;
+        } catch (Exception ex) {
+            return false;
+        }
+    }
+}
