cecf7c16aaeaabd0ac1f864cbdbec9d4409a7c37
Christian Soler <iamrelos@gmail.com>
2017-05-16 15:34:22 -0500

    Update BaseActivity.java

--- app/src/main/java/io/keypunchers/xa/app/BaseActivity.java
+++ app/src/main/java/io/keypunchers/xa/app/BaseActivity.java
@@ -235,7 +235,7 @@ public class BaseActivity extends AppCompatActivity implements NavigationView.On
         if (position == 4 && fragment == null) {
             bundle.putString("url", "http://www.xboxachievements.com/archive/achievements/");
             bundle.putString("ab_title", getResources().getString(R.string.ab_latest_achievements_title));
-            bundle.putString("header_image_url", mBanners.get(4).getImageUrl());
+            bundle.putParcelable("header", mBanners.get(4));
 
             fragment = new LatestAchievementFragment();
             fragment.setArguments(bundle);
