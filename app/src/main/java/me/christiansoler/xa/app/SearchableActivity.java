a93fd11eba15d09b2d091a17c1c50cb907b4ad57
Christian Soler <chr.solr@gmail.com>
2018-12-13 19:52:36 -0600

    Working on search UI

--- app/src/main/java/me/christiansoler/xa/app/SearchableActivity.java
+++ app/src/main/java/me/christiansoler/xa/app/SearchableActivity.java
@@ -1,27 +1,99 @@
 package me.christiansoler.xa.app;
 
-import android.app.Activity;
 import android.app.SearchManager;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.widget.Toast;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.Loader;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+
+import java.util.ArrayList;
 
 import me.christiansoler.xa.R;
+import me.christiansoler.xa.adapters.GameListAdapter;
+import me.christiansoler.xa.loaders.SearchResultLoader;
+import me.christiansoler.xa.misc.Common;
+import me.christiansoler.xa.misc.EndlessRecyclerViewScrollListener;
+import me.christiansoler.xa.models.Game;
 
-public class SearchableActivity extends Activity {
+public class SearchableActivity extends AppCompatActivity {
+
+    private final String BASE_URL = Common.BASE_URL + "/search.php";
+    private ArrayList<Game> mData = new ArrayList<>();
+    private GameListAdapter mAdapter;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_searchable);
 
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setTitle(R.string.ab_search_result);
+        }
+
+        mAdapter = new GameListAdapter(mData);
+
+        LinearLayoutManager mLinearLayoutManager = new LinearLayoutManager(this);
+        EndlessRecyclerViewScrollListener mScroller = new EndlessRecyclerViewScrollListener(mLinearLayoutManager) {
+            @Override
+            public void onLoadMore(int page, int totalItemsCount, RecyclerView view) {
+            }
+        };
+
+        RecyclerView mRvContent = (RecyclerView) findViewById(R.id.rv_search_result_list);
+        mRvContent.setAdapter(mAdapter);
+        mRvContent.setLayoutManager(mLinearLayoutManager);
+
         Intent intent = getIntent();
         if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
             String query = intent.getStringExtra(SearchManager.QUERY);
-            Toast.makeText(getApplicationContext(), query, Toast.LENGTH_SHORT).show();
+
+            getSearchResult(query);
         }
 
+    }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        switch (id) {
+            case android.R.id.home:
+                onBackPressed();
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void getSearchResult(final String query) {
+        LoaderManager.LoaderCallbacks loader = new LoaderManager.LoaderCallbacks<ArrayList<Game>>() {
+            @Override
+            public Loader<ArrayList<Game>> onCreateLoader(int id, Bundle args) {
+                return new SearchResultLoader(getApplicationContext(), BASE_URL, query);
+            }
+
+            @Override
+            public void onLoadFinished(Loader<ArrayList<Game>> loader, ArrayList<Game> data) {
+                mData.addAll(data);
+                mAdapter.notifyItemRangeChanged(mAdapter.getItemCount(), mData.size());
+            }
+
+            @Override
+            public void onLoaderReset(Loader<ArrayList<Game>> loader) {
+
+            }
+        };
+
+        getSupportLoaderManager().restartLoader(0, null, loader);
     }
 }
--- app/src/main/java/me/christiansoler/xa/loaders/SearchResultLoader.java
+++ app/src/main/java/me/christiansoler/xa/loaders/SearchResultLoader.java
@@ -8,24 +8,24 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
-import java.net.URL;
 import java.util.ArrayList;
 
 import me.christiansoler.xa.misc.Common;
 import me.christiansoler.xa.models.Game;
+
 import org.jsoup.*;
-import android.util.*;
+
 import me.christiansoler.xa.misc.Enums.*;
 
 public class SearchResultLoader extends AsyncTaskLoader<ArrayList<Game>> {
     private final String BASE_URL;
-	private final String QUERY;
+    private final String QUERY;
     private ArrayList<Game> mData = new ArrayList<>();
 
     public SearchResultLoader(Context context, String base_url, String query) {
         super(context);
         BASE_URL = base_url;
-		QUERY = query;
+        QUERY = query;
     }
 
     @Override
@@ -49,39 +49,39 @@ public class SearchResultLoader extends AsyncTaskLoader<ArrayList<Game>> {
     @Override
     public ArrayList<Game> loadInBackground() {
         try {
-			Connection.Response response = Jsoup.connect(BASE_URL)
-			.data("search", QUERY)
-			.method(Connection.Method.POST)
-			.followRedirects(true)
-			.execute();
-			
-			Document document = response.parse();
-			
-			Elements rows = document.select(".bl_la_main .divtext table tr[class~=(trA1|trA2)]");
-			
-			rows.remove(0);
-			
-			for (Element element : rows) {
+            Connection.Response response = Jsoup.connect(BASE_URL)
+                    .data("search", QUERY)
+                    .method(Connection.Method.POST)
+                    .followRedirects(true)
+                    .execute();
+
+            Document document = response.parse();
+
+            Elements rows = document.select(".bl_la_main .divtext table tr[class~=(trA1|trA2)]");
+
+            rows.remove(0);
+
+            for (Element element : rows) {
                 String artwork = element.select("img:eq(0)").attr("abs:src").replace("ico", "cover");
                 String title = element.select("td:eq(1) a:eq(0)").text().trim();
-				String platform = element.select("td:eq(3)").text().trim();
+                String platform = element.select("td:eq(3)").text().trim();
                 String ach_gs_count = element.select("td:eq(2)").text().trim();
                 String page_url = element.select("a:eq(0)").attr("href");
 
-				String test[] = ach_gs_count.split(" / ");
-				
+                String test[] = ach_gs_count.split(" / ");
+
                 Game game = new Game();
                 game.setArtwork(Common.getCoverImage(artwork));
                 game.setTitle(title);
                 game.setAchCount(test[0]);
                 game.setGsCount(test[1]);
-				game.setPlatform(platform);
+                game.setPlatform(platform);
                 game.setGamePermalink(page_url, UrlType.SEARCH_GAMES);
-				
+
                 mData.add(game);
             }
-			
-			return mData;
+
+            return mData;
         } catch (Exception ex) {
             ex.printStackTrace();
             return null;
